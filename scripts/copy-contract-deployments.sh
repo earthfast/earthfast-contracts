7e3c5676e5657ea0f764af8d54bdd6fdf93dd33c
Jorge Monroy <jlmonroy13@gmail.com>
2024-11-13 19:44:47 -0500

    Update script to copy contracts to dashboard project

--- scripts/copy-contract-deployments.sh
+++ scripts/copy-contract-deployments.sh
@@ -5,15 +5,21 @@ if [ $# -eq 0 ]; then
 fi
 
 target_repo="$1"
-if [ "$target_repo" != "cli" ] && [ "$target_repo" != "keeper" ] && [ "$target_repo" != "all" ]; then
-  echo "Invalid target repo. Please specify cli, keeper, or all"
+if [ "$target_repo" != "cli" ] && [ "$target_repo" != "keeper" ] && [ "$target_repo" != "dashboard" ] && [ "$target_repo" != "all" ]; then
+  echo "Invalid target repo. Please specify cli, keeper, dashboard, or all"
   exit 1
 fi
 
 copy_to_repo() {
   local repo=$1
-  local repo_path="../../earthfast-$repo"
-  
+  local repo_path
+
+  if [ "$repo" = "dashboard" ]; then
+    repo_path="../../earthfast-dashboard/server"
+  else
+    repo_path="../../earthfast-$repo"
+  fi
+
   for f in ../deployments/testnet-sepolia-staging/*.json; do
     jq '{address, abi}' "$f" > "$repo_path/abi/testnet-sepolia-staging/$(basename $f)"
   done
@@ -26,6 +32,7 @@ copy_to_repo() {
 if [ "$target_repo" = "all" ]; then
   copy_to_repo "cli"
   copy_to_repo "keeper"
+  copy_to_repo "dashboard"
 else
   copy_to_repo "$target_repo"
 fi
