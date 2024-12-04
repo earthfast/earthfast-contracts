10655f2a12355a4763712f86aba9d8f37dc1bf44
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2024-12-04 13:35:14 -0500

    Set epoch to next wednesday if day is Wednesday

--- bin/epochStart.ts
+++ bin/epochStart.ts
@@ -5,7 +5,7 @@ date.setUTCHours(16, 0, 0, 0);
 // Get current day (0 = Sunday, 3 = Wednesday)
 const currentDay = date.getUTCDay();
 // Calculate days to subtract to reach previous Wednesday
-const daysToSubtract = (currentDay - 3 + 7) % 7 || 7; // If result is 0, subtract 7 days
+const daysToSubtract = (currentDay - 3 + 7) % 7 || 0; // If result is 0, subtract 7 days
 date.setUTCDate(date.getUTCDate() - daysToSubtract);
 const epochStart = Math.round(date.getTime() / 1000);
 console.log(epochStart);
--- deploy/013_InitializeRegistry.ts
+++ deploy/013_InitializeRegistry.ts
@@ -38,8 +38,10 @@ async function main() {
   date.setUTCHours(16, 0, 0, 0);
   // Get current day (0 = Sunday, 3 = Wednesday)
   const currentDay = date.getUTCDay();
+  console.log(`Current day: ${currentDay}`);
   // Calculate days to subtract to reach previous Wednesday
-  const daysToSubtract = (currentDay - 3 + 7) % 7 || 7; // If result is 0, subtract 7 days
+  const daysToSubtract = (currentDay - 3 + 7) % 7 || 0; // If result is 0, subtract 7 days
+  console.log(`Days to subtract: ${daysToSubtract}`);
   date.setUTCDate(date.getUTCDate() - daysToSubtract);
   console.log(`Setting epochStart to ${date}`);
   const epochStart = Math.round(date.getTime() / 1000);
